0d1a064048d9857594cd7e6004d3d738ec90b1c2
Michael Le-Reiver <m.lereiver@gmail.com>
2018-05-16 11:19:07 -0700

    fetched items and added to store

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,9 @@
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
-  shoppingList.render();
-});
-
-store.items.push(Item.create('apples'));
-
-console.log(api.BASE_URL);
-
-api.createItem('grapes', (newItem) => {
   api.getItems((items) => {
-    console.log(items);
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
   });
 });
+
